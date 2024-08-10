420d617de83ccc4a27bdefb7ecd7e3b358b931a4
Kirill Pekarskiy <kipe122006@gmail.com>
2024-08-10 22:24:20 +0300

    Доработка функционала перехода по ссылкам внутри страницы

--- script/modules/scrollLink.js
+++ script/modules/scrollLink.js
@@ -1,14 +1,22 @@
 const $links = document.querySelectorAll(".link");
 const $buttons = document.querySelectorAll(".button");
+const $header = document.querySelector(".header");
+const isStickyHeader = getComputedStyle($header).position === "sticky";
+const headerHeight = $header.offsetHeight;
 
 $buttons.forEach($button => {
     $button.addEventListener("click", clickButtonHandler);
 
     function clickButtonHandler() {
         const href = this.dataset.href;
-        if (href) document.querySelector(href).scrollIntoView({behavior: "smooth"});
+        if (href) window.scrollTo({top: getGlobalOffsetTop(document.querySelector(href)) - (isStickyHeader ? headerHeight : 0), behavior: "smooth"});
     }
 })
+function getGlobalOffsetTop($block) {
+    if (!$block) return 0;
+    const offset = $block.offsetTop;
+    return offset + getGlobalOffsetTop($block.offsetParent);
+}
 
 $links.forEach($link => {
     $link.addEventListener("click", clickLinkHandler);
@@ -16,10 +24,8 @@ $links.forEach($link => {
     function clickLinkHandler() {
         const href = this.dataset.href;
         if (href) {
-            if (document.querySelector(".burger") && document.querySelector(".burger").classList.contains("burger_active")) {
-                toggleButtonBurgerHandler();
-            }
-            document.querySelector(href).scrollIntoView({behavior: "smooth"});
+            if (document.querySelector(".burger") && document.querySelector(".burger").classList.contains("burger_active")) toggleButtonBurgerHandler();
+            window.scrollTo({top: getGlobalOffsetTop(document.querySelector(href)) - (isStickyHeader ? headerHeight : 0), behavior: "smooth"});
         }
     }
 })
